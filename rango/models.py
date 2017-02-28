9e98510074f11eb1a7ea392e598650d7f41b8301
JosejeSinohui <josejesinohui@gmail.com>
2017-02-28 01:56:17 -0700

    added bootstrap base and reformatted some code

--- rango/models.py
+++ rango/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.template.defaultfilters import slugify
 
+
 # Create your models here.
 
 class Category(models.Model):
@@ -35,7 +36,6 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User)
     website = models.URLField(blank=True)
     picture = models.ImageField(upload_to='profile_images', blank=True)
-
 
     def __str__(self):
         return self.user.username
