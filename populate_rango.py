0c55f6a11990eaa67bcfa8ef7e4d407e424160d0
JosejeSinohui <josejesinohui@gmail.com>
2017-04-30 23:01:42 -0700

    Added profile functionality

--- populate_rango.py
+++ populate_rango.py
@@ -68,7 +68,6 @@ def populate():
 
 def add_page(cat, title, url, views):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
-    p.url = url
     p.views = views
     p.save()
     return p
--- tango_with_django/urls.py
+++ tango_with_django/urls.py
@@ -20,12 +20,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 from rango import views
-from rango.views import MyRegistrationView
+from rango.views import RangoRegistrationView
 
 urlpatterns = [
                   url(r'^admin/', admin.site.urls),
                   url(r'^$', views.index, name='index'),
                   url(r'^rango/', include('rango.urls')),
-                  url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+                  url(r'^accounts/register/$', RangoRegistrationView.as_view(), name='registration_register'),
                   url(r'^accounts/', include('registration.backends.simple.urls'))
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
